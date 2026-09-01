3a8214962f3cca6c7c1a3a13ec60eeafb4d14c0f
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-01 15:01:21 +0300

    completed 02-express

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/api/v1/cats", (req, res) => {
+  const cat = {
+    cat_id: 777,
+    name: "Kitty",
+    birthdate: "1/9/2020",
+    weight: "4.2kg",
+    owner: "Joshua",
+    image:
+      "https://plus.unsplash.com/premium_photo-1667030474693-6d0632f97029?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  };
+  res.json(cat);
+});
+
+app.use("/public", express.static("public"));
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
